cd9195bf1a1af77ab270ff2b06416a817a6895bf
JB <jean-bernard.laguerre@laplateforme.io>
2022-10-21 09:56:47 +0200

    script d'installation/configuration proftpd

---
+++ Job_7/ftp_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt install proftpd
+apt install openssh-server
+
+cat proftpd.conf > /etc/proftpd/proftpd.conf
+cat tls.conf > /etc/proftpd/tls.conf
+cat modules.conf > /etc/proftpd/modules.conf
+
+#clé et certificat ssl
+openssl genrsa -out /etc/ssl/private/proftpd.key 2048
+openssl req -new -x509 -days 3650 -key /etc/ssl/private/proftpd.key -out /etc/ssl/certs/proftpd.crt
+
+servicectl proftpd restart
