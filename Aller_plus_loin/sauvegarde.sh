425730ce8c859a0f9b6128aedd501a225bc46540
JB <jean-bernard.laguerre@laplateforme.io>
2022-10-21 17:30:06 +0200

    Archivage et envoi vers une autre machine

--- Aller_plus_loin/sauvegarde.sh
+++ Aller_plus_loin/sauvegarde.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+cd#!/bin/bash
 
-sudo rsync -aAX / --delete /backup_$(date +"%d-%m-%Y_%H:%M") "backup"
+sudo rsync -aAX / --delete /backup_$(date +"%d-%m-%Y_%H:%M") test@10.0.2.5:/Backup
