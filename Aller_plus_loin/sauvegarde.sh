1eeee6e844ac67ce7ecf765c68d90b72990839c4
JB <jean-bernard.laguerre@laplateforme.io>
2022-10-24 10:04:13 +0200

    correction faute de frappe

--- Aller_plus_loin/sauvegarde.sh
+++ Aller_plus_loin/sauvegarde.sh
@@ -1,3 +1,3 @@
-cd#!/bin/bash
+#!/bin/bash
 
-sudo rsync -aAX / --delete /backup_$(date +"%d-%m-%Y_%H:%M") test@10.0.2.5:/Backup
+sudo rsync -aAX / --delete /"backup_$(date +"%d-%m-%Y_%H:%M")" test@10.0.2.5:/Backup
